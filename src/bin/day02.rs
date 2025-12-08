0fd86475b7d7cb05c3158409516ae48f20fa5c34
Mateusz Swirski <mateusz@payzilch.com>
2025-12-08 22:53:06 +0000

    Refactor Day 2 solution to update part1 for invalid ID calculation. Change return type to u64 and enhance logic for identifying invalid IDs based on repeated digit patterns. Update task description with detailed examples and clarify rules for invalid IDs.

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -21,18 +21,18 @@ fn main() {
     }
 }
 
-fn part1(input: &str) -> usize {
+fn part1(input: &str) -> u64 {
     // fetch the first line from the input, split by comma and store as a vector
     let first_line = input.lines().next().unwrap_or("");
     let items: Vec<&str> = first_line.split(',').collect();
-
+    let mut sum_of_invalid_ids: u64 = 0;
     // iterate over the items
     for item in &items {
         // split item by hyphen
         let parts: Vec<&str> = item.split('-').collect();
-        let start: i32 = parts[0].parse().unwrap_or(0);
-        let end: i32 = parts[1].parse().unwrap_or(0);
-        let invalid_ids: Vec<i32> = vec![];
+        let start: u64 = parts[0].parse().unwrap_or(0);
+        let end: u64 = parts[1].parse().unwrap_or(0);
+        let mut invalid_ids: Vec<u64> = vec![];
 
         // iterate from start to end, end inclusive
         for number in start..=end {
@@ -48,17 +48,21 @@ fn part1(input: &str) -> usize {
                 // extract the first half and second half of the number
                 // e.g. 1234 -> first_half = 12, second_half = 34
                 // 4 * 1
-                let mut second_half = number / (10_i32.pow((digit_count) / 2));
-                let mut first_half = number - second_half;
-                println!(
-                    "Number: {}, Digit Count: {}, First half: {}, Second half: {}",
-                    number, digit_count, first_half, second_half
-                );
+                let mut decimal_multiplier: u64 = 10_u64.pow(digit_count / 2);
+                let mut first_half: u64 = number / decimal_multiplier;
+                let mut second_half: u64 = number - first_half * decimal_multiplier;
+                
+                if first_half == second_half {
+                    println!("Invalid ID: {}", number);
+                    invalid_ids.push(number);
+                }
             }
         }
+
+        sum_of_invalid_ids = sum_of_invalid_ids + invalid_ids.iter().sum::<u64>();
     }
 
-    items.len()
+    sum_of_invalid_ids
 }
 
 // function extracting first part and last part of a number
