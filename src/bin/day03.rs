327d1b1a58711c8d427c82fa98b3eae4118fc283
Mateusz Swirski <mateusz@payzilch.com>
2025-12-12 12:56:16 +0000

    Implement Day 3 solution to calculate maximum joltage from battery banks. Update part1 to sum the largest joltage for each bank and enhance find_largest_joltage function. Revise task description with detailed examples and clarify requirements.

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -22,7 +22,37 @@ fn main() {
 }
 
 fn part1(input: &str) -> usize {
-    input.lines().count()
+    // iterate over each line
+    let mut sum: usize = 0;
+    for line in input.lines() {
+        sum += find_largest_joltage(line);
+    }
+
+    sum
+}
+
+fn find_largest_joltage(line: &str) -> usize {
+    let c: Vec<u32> = line.chars()
+        .filter_map(|c| c.to_digit(10))
+        .collect();
+
+    let mut pointer_1 = 0;
+    let mut pointer_2 = 1;
+    let mut max_joltage = 0;
+
+    while pointer_1 < c.len() - 1 {
+        while pointer_2 < c.len() {
+            let joltage = c[pointer_1]*10 + c[pointer_2];
+            if joltage > max_joltage {
+                max_joltage = joltage;
+            }
+            pointer_2 += 1;
+        }
+        pointer_1 += 1;
+        pointer_2 = pointer_1 + 1;
+    }
+
+    max_joltage as usize
 }
 
 fn part2(input: &str) -> usize {
@@ -35,15 +65,14 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let input = ""; // Provide a test input
-        let result = part1(input);
-        assert_eq!(result, 0); // Provide the expected result
+        let input = "12";
+        assert_eq!(find_largest_joltage(input), 12); // Replace 0 with the expected result
     }
 
     #[test]
-    fn test_part2() {
-        let input = ""; // Provide a test input
-        let result = part2(input);
-        assert_eq!(result, 0); // Provide the expected result
+    fn test_part1_1() {
+        let input = "12345";
+        assert_eq!(find_largest_joltage(input), 45); // Replace 0 with the expected result
     }
+
 }
